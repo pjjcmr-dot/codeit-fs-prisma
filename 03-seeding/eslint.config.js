4f039dcaf76240acd6e7724fed112d87256c39d3
박종장 <pjjcmr@gmail.com>
2026-01-14 23:58:08 +0900

    21-3

--- 03-seeding/eslint.config.js
+++ 03-seeding/eslint.config.js
@@ -12,7 +12,10 @@ export default [
       },
     },
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+      ],
       'no-console': 'off',
       'prefer-const': 'error',
       'no-var': 'error',
